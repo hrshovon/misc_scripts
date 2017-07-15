798ae0930009c7af08313020a8dc5e8a2ed17656
S.M. Hasanur Rashid <hrshovon@gmail.com>
2017-07-15 06:31:35 +0600

    Update install_avro_keyboard.sh
    Added instructions for install_avro_phonetic

--- install_avro_keyboard.sh
+++ install_avro_keyboard.sh
@@ -6,3 +6,11 @@ cd ibus-avro
 aclocal && autoconf && automake --add-missing
 ./configure --prefix=/usr
 sudo make install
+
+#Now, go to System>Preferences>Personal>Language Support (I am running Ubuntu Mate,So just find out where language support is for your distro)
+#Select keyboard input method as iBus
+#Save and exit
+#Go to iBus Preferences (System>Preferences>Others>iBus Preferences) and select input method tab. There click Add>Bangla>Avro Phonetic.
+#After that, click Add. You may have to select the three dots to get Bangla/Bengali.
+#Restart your PC.
+#Avro should be available at the top right corner. Click on EN and select Avro phonetic when needed. Enjoy!
